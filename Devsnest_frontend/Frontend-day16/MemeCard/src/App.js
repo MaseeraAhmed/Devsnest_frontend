6cd715190c0735ee510fcc336ba85b183b0c4e4d
Maseera Ahmed <71185583+MaseeraAhmed@users.noreply.github.com>
2021-07-07 03:41:23 +0530

    Chess-board  & Meme Card

---
+++ Devsnest_frontend/Frontend-day16/MemeCard/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+    return(
+        <div className="card">
+        <div className="image">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf8hhkz3l8NBqSWVP7_z_ReuQwIiZDXDNAQA&usqp=CAU"
+            alt="meme"
+          ></img>
+        </div>
+        <h1>Meme card😹</h1>
+        <div className="cardDescription">
+        </div>
+      </div>
+    );
+}
+
+export default App;
