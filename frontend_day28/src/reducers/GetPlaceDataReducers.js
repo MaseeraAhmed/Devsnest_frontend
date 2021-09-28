02b6b672d9b510a59c73d78b87f2977f31f3fdbe
Maseera Ahmed <madihaahmed2017@gmail.com>
2021-09-29 01:14:03 +0530

    done

---
+++ frontend_day28/src/reducers/GetPlaceDataReducers.js
@@ -0,0 +1,10 @@
+const GetPlaceDataReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "GET_PLACE_DATA":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default GetPlaceDataReducer;
